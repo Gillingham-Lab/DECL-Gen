40cf793efc633f1d25b470d2da8e09c04aba3283
Vassyli <basilius.sauter@gmail.com>
2019-04-26 10:12:07 +0200

    Adds documentation to DECLGen/codon.py

--- DECLGen/codon.py
+++ DECLGen/codon.py
@@ -1,27 +1,45 @@
 from typing import NewType, Union
 
 class CodonConfig:
+    """ Defines which base translate to which digit."""
     bases = "ATGC"
 
-
+# Define a few types for type hinting.
 bases = CodonConfig.bases
 CodonInt = NewType("CodonInt", int)
 CodonStr = NewType("CodonStr", str)
 CodonType = NewType("CodonType", Union[CodonStr, CodonInt])
 
 def normalize(index: CodonType) -> CodonInt:
+    """
+    Normalizes a CodonType to a CodonInt.
+
+    This is a helper method that can be utilized to make methods accept both CodonInt and CodonStr. It always decodes
+    a CodonStr to a CodonInt, but does not return a CodonStr. Additionally, it also tries to convert string digits to
+    real digits.
+    :param index: An input to convert.
+    :return: The decoded codon integer.
+    """
     try:
+        # Try to convert to integer
         if type(index) == str and index.isdigit():
             index = int(index)
     except ValueError:
+        # If it fails, we leave index alone since its a string.
         pass
 
+    # If index is a string, we assume its a CodonStr and decode it to an integer.
     if type(index) == str:
         index = decode(index)
 
     return index
 
 def reverse(codon: str) -> str:
+    """
+    Returns the reverse complement of a given Sequence.
+    :param codon: Input sequence
+    :return: Reverse complement of the input sequence.
+    """
     codon = codon.upper()
     codon = codon.replace("A", "t")
     codon = codon.replace("C", "g")
