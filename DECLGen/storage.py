a26643f8c9f2e9f07386744faeea110f050462de
Vassyli <basilius.sauter@gmail.com>
2019-05-02 08:20:34 +0200

    Adds documentation to DECLGen/storage.py

--- DECLGen/storage.py
+++ DECLGen/storage.py
@@ -4,6 +4,10 @@ from .library import Library
 
 
 class Storage:
+    """
+    This class stores all information from a library and is the class that gets pickled.
+    """
+
     # This sequence defines the number-value of each base
     bases = "ATGC"
     # The raw template as given by the user, sanitized
@@ -16,10 +20,17 @@ class Storage:
     library = None
 
     def __init__(self, advanced_anchors: bool, superset_categories: bool):
+        """
+        Constructor.
+        :param advanced_anchors: True if advanced-anchor-mode should be utilized.
+        :param superset_categories: True of superset categories should be enabled.
+        """
+        # Initialize empty values.
         self.raw_template = None
         self.smiles_template = None
         self.dna_template = None
 
+        # Create the (empty) library
         self.library = Library(
             self,
             advanced_anchors=advanced_anchors,
@@ -27,6 +38,13 @@ class Storage:
         )
 
     def set_template(self, template_string):
+        """
+        Sets the template.
+
+        This method sets the molecular scaffold if it has not yet been set. It also sets the R-Groups.
+        :param template_string:
+        :return:
+        """
         if self.raw_template is not None:
             raise LibraryTemplateException("Cannot re-set the template via Storage.set_template() after it has been initialized.")
 
@@ -35,5 +53,5 @@ class Storage:
 
         self.library.change_template(template_string)
 
-        # Sets the libraries R-Groups
+        # Sets the libraries R-Groups. This cannot ever be changed.
         self.library.set_anchors(template.get_anchors(template_string))
